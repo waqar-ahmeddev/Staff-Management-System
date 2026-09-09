9b6602b87555aa23b7880046ed8a89af1d8f909c
Waqar Ahmed <24pwbcs1370@uetpeshawar.edu.pk>
2026-09-09 15:29:09 +0500

    fix speeling mistakes on userroutes.js

--- backend/routers/userroutes.js
+++ backend/routers/userroutes.js
@@ -6,7 +6,7 @@ import {
   logoutUser,
 } from "../Controllers/UserControllers.js";
 
-import authMiddleware from "../middleware/authMiddleware.js";
+import authMiddleware from "../middleware/authmiddleware.js";
 
 const router = express.Router();
 
